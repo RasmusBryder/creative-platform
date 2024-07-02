1d9a1d7681f1009309f0110fe5cb71b3b9c74fa1
Rasmus Sylvester Bryder <rollosb@gmail.com>
2024-07-02 11:54:18 +0200

    Added Campaign repo

---
+++ src/CreativePlatform.Order/Infrastructure/Campaigns/CampaignRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using CreativePlatform.Order.Domain.Campaigns;
+
+namespace CreativePlatform.Order.Infrastructure.Campaigns;
+
+public interface ICampaignRepository
+{
+    Task<CampaignResource?> GetAsync(Guid id);
+}
+
+public class CampaignRepositoryStub : ICampaignRepository
+{
+    public Task<CampaignResource?> GetAsync(Guid id)
+    {
+
+        return Task.FromResult(new CampaignResource(id,
+            Faker.Company.CatchPhrase(),
+            "ToBeDefined",
+            [
+                "Websites", "Social Media", "Owned"
+            ],
+            ["ToBeDefined"]))!;
+    }
+
+    private static readonly Faker Faker = new();
+}
